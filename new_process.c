d6da85a06c7722471d70ead6167c4c743de46231
LauraW9365 <laura.wafula@gmail.com>
2023-05-25 03:43:47 -0700

    simple_shell

--- new_process.c
+++ new_process.c
@@ -2,37 +2,39 @@
 
 int main(void)
 {
-    pid_t pid;
-    int status;
+	pid_t pid;
+	int status;
 
-    pid = fork();
-    if (pid == 0)
-    {
-        /* Child process */
-        if (execl("/bin/ls", "ls", "-l", NULL) == -1)
-        {
-            perror("execl");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else if (pid < 0)
-    {
-        /* Forking error */
-        perror("fork");
-    }
-    else
-    {
-        /* Parent process */
-        do
-        {
-            /* Wait for child process to complete */
-            if (waitpid(pid, &status, WUNTRACED) == -1)
-            {
-                perror("waitpid");
-            }
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+	pid = fork();
+	if (pid == 0)
 
-    return 0;
+	{
+		/* Child process */
+		if (execl("/bin/ls", "ls", "-l", NULL) == -1)
+		{
+			error("execl");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid < 0)
+	{
+		/* Forking error */
+		perror("fork");
+	}
+	else
+	{
+		/* Parent process */
+		do
+
+		{
+			/* Wait for child process to complete */
+			if (waitpid(pid, &status, WUNTRACED) == -1)
+			{
+				perror("waitpid");
+			}
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	return (0);
 }
 
--- split_line.c
+++ split_line.c
@@ -16,59 +16,59 @@
  */
 char **split_line(char *line, const char *delimiter)
 {
-    int token_count = 0;
-    char **tokens = malloc(MAX_TOKENS * sizeof(char *));
-    char *token;
+	int token_count = 0;
+	char **tokens = malloc(MAX_TOKENS * sizeof(char *));
+	char *token;
 
-    if (tokens == NULL)
-    {
-        fprintf(stderr, "Allocation error\n");
-        exit(EXIT_FAILURE);
-    }
+	if (tokens == NULL)
+	{
+		fprintf(stderr, "Allocation error\n");
+		exit(EXIT_FAILURE);
+	}
 
-    token = strtok(line, delimiter);
-    while (token != NULL)
-    {
-        tokens[token_count] = strdup(token);
-        token = strtok(NULL, delimiter);
-        token_count++;
+	token = strtok(line, delimiter);
+	while (token != NULL)
+	{
+		tokens[token_count] = strdup(token);
+		token = strtok(NULL, delimiter);
+		token_count++;
 
-        if (token_count >= MAX_TOKENS)
-        {
+		if (token_count >= MAX_TOKENS)
+		{
             /* Increase the size of tokens array if needed */
-            char **new_tokens = realloc(tokens, (MAX_TOKENS * 2) * sizeof(char *));
-            if (new_tokens == NULL)
-            {
-                fprintf(stderr, "Allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-            tokens = new_tokens;
-            MAX_TOKENS *= 2;
-        }
-    }
-    tokens[token_count] = NULL;
+			char **new_tokens = realloc(tokens, (MAX_TOKENS * 2) * sizeof(char *));
 
-    return tokens;
+			if (new_tokens == NULL)
+			{
+				fprintf(stderr, "Allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+			tokens = new_tokens;
+			MAX_TOKENS *= 2;
+		}
+	}
+	tokens[token_count] = NULL;
+
+	return (tokens);
 }
 
 int main(void)
 {
-    char input[] = "This is a sample input string";
-    char **tokens;
+	char input[] = "This is a sample input string";
+	char **tokens;
 
-    tokens = split_line(input, " ");
-    if (tokens != NULL)
-    {
-        int i = 0;
-        while (tokens[i] != NULL)
-        {
-            printf("Token %d: %s\n", i, tokens[i]);
-            free(tokens[i]);
-            i++;
-        }
-        free(tokens);
-    }
-
-    return 0;
+	tokens = split_line(input, " ");
+	if (tokens != NULL)
+	{
+		int i = 0;
+		while (tokens[i] != NULL)
+		{
+			printf("Token %d: %s\n", i, tokens[i]);
+			free(tokens[i]);
+			i++;
+		}
+		free(tokens);
+	}
+	return (0);
 }
 

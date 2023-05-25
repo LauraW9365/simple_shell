069979695fcd74f76d565b5e8efcdc5324dc51c9
LauraW9365 <laura.wafula@gmail.com>
2023-05-25 02:52:19 -0700

    betty

--- cd.c
+++ cd.c
@@ -1,22 +1,26 @@
 #include "shell.h"
 
-int main() {
-    const char* new_directory = "/path/to/new_directory";
+int main(void)
+{
+	const char *new_directory = "/path/to/new_directory";
 
-    if (chdir(new_directory) != 0) {
-        perror("chdir");
-        return 1;
-    }
+	if (chdir(new_directory) != 0)
+	{
+		perror("chdir");
+		return (1);
+	}
 
-    // Print the updated working directory
-    char cwd[1024];
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        printf("Current directory: %s\n", cwd);
-    } else {
-        perror("getcwd");
-        return 1;
-    }
+	char cwd[1024];
 
-    return 0;
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+	{
+		printf("Current directory: %s\n", cwd);
+	} else
+	{
+		perror("getcwd");
+		return (1);
+	}
+
+	return (0);
 }
 
